2aa815e8fa70b54e930342ccfc58d8199025e01f
domnet-webdesign <domnetwebdesign@gmail.com>
2019-12-16 22:31:44 +0100

    initial commit

---
+++ Portfolio/dist/js/main.js
@@ -0,0 +1,12 @@
+function openSlideMenu (){
+    document.getElementById("side-menu").style.display = "block";
+
+    document.getElementById("open").style.display = "none";
+    //getOpenClose();
+}
+function closeSlideMenu (){
+    document.getElementById("side-menu").style.display = "none";
+
+    document.getElementById("open").style.display = "block";
+    //getOpenClose();
+}
